11544316c8585d1a7547d004b8da42124568bdba
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-07-26 10:29:46 +0700

    updated cors middleware

--- internal/infrastructure/middleware/cors.go
+++ internal/infrastructure/middleware/cors.go
@@ -1,10 +1,27 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	emiddleware "github.com/labstack/echo/v4/middleware"
 )
 
 func CORS() echo.MiddlewareFunc {
-	return emiddleware.CORS()
+	return emiddleware.CORSWithConfig(emiddleware.CORSConfig{
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		AllowHeaders: []string{
+			echo.HeaderAccept,
+			echo.HeaderAcceptEncoding,
+			echo.HeaderAccessControlAllowHeaders,
+			echo.HeaderAuthorization,
+			echo.HeaderContentType,
+			echo.HeaderContentLength,
+			echo.HeaderOrigin,
+			echo.HeaderXCSRFToken,
+		},
+		MaxAge: 1800,
+	})
 }
