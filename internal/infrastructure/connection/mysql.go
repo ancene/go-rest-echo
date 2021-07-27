20cdfcb2bef8c6f0a87d23ac49c2737f7ecbff8f
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-07-27 10:48:09 +0700

    update func NewMysqlConnection

--- internal/infrastructure/connection/mysql.go
+++ internal/infrastructure/connection/mysql.go
@@ -1,7 +1,29 @@
 package connection
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
 
-func NewMysqlConnection() (*sql.DB, error) {
-	return nil, nil
+	"github.com/ancene/go-rest-echo/internal/domain"
+)
+
+func NewMysqlConnection(cfg *domain.Configuration) (*sql.DB, error) {
+	if cfg == nil {
+		return nil, errors.New("configuration is nil")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", cfg.Mysql.Username, cfg.Mysql.Password, cfg.Mysql.Host, cfg.Mysql.Port, cfg.Mysql.Database)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
